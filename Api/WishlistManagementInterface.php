103f53e70ae399b579f24c9c3f3371b460880297
pkumar <pkumar@raveinfosys.com>
2020-10-05 18:00:47 +0530

    Add product to Wishlist API

---
+++ Api/WishlistManagementInterface.php
@@ -0,0 +1,29 @@
+<?php
+namespace Aheadworks\MobileAppConnector\Api;
+
+/**
+ * Interface WishlistManagementInterface
+ * @api
+ */
+interface WishlistManagementInterface
+{
+
+    /**
+     * Return Wishlist items.
+     *
+     * @param int $customerId
+     * @return array
+     */
+    public function getWishlistForCustomer($customerId);
+
+    /**
+     * Return Added wishlist item.
+     *
+     * @param int $customerId
+     * @param int $productId
+     * @return array
+     *
+     */
+    public function addWishlistForCustomer($customerId,$productId);
+
+}
