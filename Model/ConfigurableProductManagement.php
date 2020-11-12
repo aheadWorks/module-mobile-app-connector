0b4f25759c8b86118b44a38f2bbd4a96842a6779
Ravendra Gupta <rgupta>
2020-11-12 20:49:11 +0530

    MAC-9 :removed the duplicated logic, replaced qualifier with the import,removed unnssary argument and removed the unnessary transformation

--- Model/ConfigurableProductManagement.php
+++ Model/ConfigurableProductManagement.php
@@ -3,11 +3,12 @@
 namespace Aheadworks\MobileAppConnector\Model;
 
 use Aheadworks\MobileAppConnector\Api\ConfigurableProductManagementInterface;
-use Aheadworks\MobileAppConnector\Model\Product\Image\Resolver;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\ConfigurableProduct\Model\LinkManagement;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Framework\Exception\InputException;
+use Aheadworks\MobileAppConnector\Model\Product\Resolver;
 
 /**
  * Class ConfigurableProductManagement
@@ -18,14 +19,8 @@ class ConfigurableProductManagement implements ConfigurableProductManagementInte
     /**#@+
      * Constants defined for keys of the data array.
      */
-    const DATA = 'data';
     const VARIATION = 'variation';
     const OPTIONS ='configurable_options';
-    const ID = 'id';
-    const MIN_PRICE = 'min_price';
-    const MAX_PRICE = 'max_price';
-    const FINAL_PRICE = 'final_price';
-    const IMAGE = 'product_image';
 
     /**
      * @var ProductRepositoryInterface
@@ -40,23 +35,24 @@ class ConfigurableProductManagement implements ConfigurableProductManagementInte
     /**
      * @var Resolver
      */
-    protected $imageResolver;
+    private $productResolver;
 
     /**
      * Constructor
      *
      * @param ProductRepositoryInterface $productRepository
      * @param LinkManagement $linkManagement
-     * @param Resolver $imageResolver
+     * @param Resolver $productResolver
      */
     public function __construct(
         ProductRepositoryInterface $productRepository,
         LinkManagement $linkManagement,
-        Resolver $imageResolver
+        Resolver $productResolver
+
     ) {
         $this->productRepository = $productRepository;
         $this->linkManagement = $linkManagement;
-        $this->imageResolver = $imageResolver;
+        $this->productResolver = $productResolver;
     }
 
     /**
@@ -70,64 +66,32 @@ class ConfigurableProductManagement implements ConfigurableProductManagementInte
         /** @var \Magento\Catalog\Model\Product $product */
         $product = $this->productRepository->get($sku);
 
-        if ($product->getTypeId() != \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE) {
+        if ($product->getTypeId() != Configurable::TYPE_CODE) {
             return [];
         }
         $childrenList = $this->linkManagement->getChildren($sku);
-        $productAttributeOptions = $product->getTypeInstance(true)->getConfigurableAttributesAsArray($product);
-        $options = [];
-        foreach ($productAttributeOptions as $productAttribute) {
-            $options[] = $productAttribute;
-        }
-
+        $productAttributeOptions = $product->getTypeInstance()->getConfigurableAttributesAsArray($product);
         $variants = [];
         foreach ($childrenList as $key => $child) {
             $variants[$key] = ['product' => $child->getData()];
         }
 
         $data = [
-                self::ID => $product->getId(),
+                Resolver::ID => $product->getId(),
                 ProductInterface::SKU => $product->getSku(),
                 ProductInterface::NAME => $product->getName(),
                 ProductInterface::PRICE => $product->getPrice(),
                 ProductInterface::TYPE_ID => $product->getTypeId(),
-                self::MIN_PRICE => $this->getMinimumPrice($product),
-                self::MAX_PRICE => $this->getMaximumPrice($product),
-                self::FINAL_PRICE => $product->getFinalPrice(),
-                self::IMAGE => $this->getProductImageUrl($product),
-                self::OPTIONS => $options,
+                Resolver::MIN_PRICE => $this->productResolver->getMinimumPrice($product),
+                Resolver::MAX_PRICE => $this->productResolver->getMaximumPrice($product),
+                Resolver::FINAL_PRICE => $product->getFinalPrice(),
+                Resolver::IMAGE => $this->productResolver->getProductImageUrl($product),
+                self::OPTIONS => $productAttributeOptions,
                 self::VARIATION => $variants
             ];
 
         $productsData[] = $data;
         return $productsData;
     }
-    /**
-     * @param ProductInterface $product
-     * @return string $image
-     */
-    protected function getProductImageUrl($product)
-    {
-        return $this->imageResolver->getProductImageUrl($product, 'category_page_grid');
-    }
 
-    /**
-     * Return product min price
-     * @param ProductInterface $product
-     * @return double
-     */
-    private function getMinimumPrice($product)
-    {
-        return $product->getPriceInfo()->getPrice('final_price')->getMinimalPrice()->getValue();
-    }
-
-    /**
-     * Return product max price
-     * @param ProductInterface $product
-     * @return double
-     */
-    private function getMaximumPrice($product)
-    {
-        return $product->getPriceInfo()->getPrice('final_price')->getMaximalPrice()->getValue();
-    }
 }
--- Model/RelatedProductsManagement.php
+++ Model/RelatedProductsManagement.php
@@ -6,13 +6,13 @@
 namespace Aheadworks\MobileAppConnector\Model;
 
 use Aheadworks\MobileAppConnector\Api\RelatedProductsRepositoryInterface;
-use Aheadworks\MobileAppConnector\Model\Product\Image\Resolver;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product\Link;
 use Magento\Catalog\Model\Product\LinkFactory;
 use Magento\Catalog\Model\Product\Visibility;
 use Magento\Framework\Exception\InputException;
+use Aheadworks\MobileAppConnector\Model\Product\Resolver;
 
 /**
  * Class RelatedProductsManagement
@@ -20,11 +20,6 @@ use Magento\Framework\Exception\InputException;
  */
 class RelatedProductsManagement implements RelatedProductsRepositoryInterface
 {
-    const ID = 'id';
-    const MIN_PRICE = 'min_price';
-    const MAX_PRICE = 'max_price';
-    const FINAL_PRICE = 'final_price';
-    const IMAGE = 'product_image';
 
     /**
      * @var ProductRepositoryInterface
@@ -34,7 +29,7 @@ class RelatedProductsManagement implements RelatedProductsRepositoryInterface
     /**
      * @var Resolver
      */
-    protected $imageResolver;
+    protected $productResolver;
 
     /**
      * @var LinkFactory
@@ -48,18 +43,18 @@ class RelatedProductsManagement implements RelatedProductsRepositoryInterface
 
     /**
      * @param ProductRepositoryInterface $productRepository
-     * @param Resolver $imageResolver
+     * @param Resolver $productResolver
      * @param LinkFactory $linkFactory
      * @param Visibility $productVisibility
      */
     public function __construct(
         ProductRepositoryInterface $productRepository,
-        Resolver $imageResolver,
+        Resolver $productResolver,
         LinkFactory $linkFactory,
         Visibility $productVisibility
     ) {
         $this->productRepository = $productRepository;
-        $this->imageResolver = $imageResolver;
+        $this->productResolver = $productResolver;
         $this->linkFactory = $linkFactory;
         $this->productVisibility = $productVisibility;
     }
@@ -88,15 +83,15 @@ class RelatedProductsManagement implements RelatedProductsRepositoryInterface
             $productsData = [];
             foreach ($relatedProducts as $relatedProduct) {
                 $data = [
-                    self::ID => $relatedProduct->getId(),
+                    Resolver::ID => $relatedProduct->getId(),
                     ProductInterface::SKU => $relatedProduct->getSku(),
                     ProductInterface::NAME => $relatedProduct->getName(),
                     ProductInterface::PRICE => $relatedProduct->getPrice(),
                     ProductInterface::TYPE_ID => $relatedProduct->getTypeId(),
-                    self::MIN_PRICE => $this->getMinimumPrice($relatedProduct),
-                    self::MAX_PRICE => $this->getMaximumPrice($relatedProduct),
-                    self::FINAL_PRICE => $relatedProduct->getFinalPrice(),
-                    self::IMAGE => $this->getProductImageUrl($relatedProduct),
+                    Resolver::MIN_PRICE => $this->productResolver->getMinimumPrice($relatedProduct),
+                    Resolver::MAX_PRICE => $this->productResolver->getMaximumPrice($relatedProduct),
+                    Resolver::FINAL_PRICE => $relatedProduct->getFinalPrice(),
+                    Resolver::IMAGE => $this->productResolver->getProductImageUrl($relatedProduct),
 
                 ];
                 $productsData[] = $data;
@@ -114,34 +109,5 @@ class RelatedProductsManagement implements RelatedProductsRepositoryInterface
     private function getLinkType(): int
     {
         return Link::LINK_TYPE_RELATED;
-    }
-
-    /**
-     * @param ProductInterface $relatedProduct
-     * @return string $image
-     */
-    protected function getProductImageUrl($relatedProduct)
-    {
-        return $this->imageResolver->getProductImageUrl($relatedProduct, 'category_page_grid');
-    }
-
-    /**
-     * Return product min price
-     * @param ProductInterface $relatedProduct
-     * @return double
-     */
-    private function getMinimumPrice($relatedProduct)
-    {
-        return $relatedProduct->getPriceInfo()->getPrice('final_price')->getMinimalPrice()->getValue();
-    }
-
-    /**
-     * Return product max price
-     * @param ProductInterface $relatedProduct
-     * @return double
-     */
-    private function getMaximumPrice($relatedProduct)
-    {
-        return $relatedProduct->getPriceInfo()->getPrice('final_price')->getMaximalPrice()->getValue();
     }
 }
