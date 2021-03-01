86f3e9f70da80897a17f338c35b193a7e14b039f
Roman Levitsky <levitsky@raveinfosys.com>
2021-03-01 16:38:48 +0300

    BUILDIFYM2-78 Only one column structure is supported

---
+++ Block/ThirdParty/Buildify/Page/Preview.php
@@ -0,0 +1,41 @@
+<?php
+namespace Aheadworks\MobileAppConnector\Block\ThirdParty\Buildify\Page;
+
+use \Aheadworks\MobileAppConnector\ViewModel\ThirdParty\Buildify\Page\Preview as ViewModel;
+use Magento\Framework\View\Element\Template;
+
+/**
+ * Class Preview
+ * @package Aheadworks\MobileAppConnector\Block\ThirdParty\Buildify\Page
+ */
+class Preview extends Template
+{
+    /**
+     * @var ViewModel
+     */
+    private $viewModel;
+
+    /**
+     * @param Template\Context $context
+     * @param ViewModel $viewModel
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        ViewModel $viewModel,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->viewModel = $viewModel;
+    }
+
+    /**
+     * Retrieve view model
+     *
+     * @return ViewModel
+     */
+    public function getViewModel()
+    {
+        return $this->viewModel;
+    }
+}
