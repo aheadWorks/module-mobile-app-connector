c8b72a798478951b4c77abfb0e66d52da734b3ea
Yuri Silchenkov <silchenkov@aheadworks.com>
2022-08-15 17:00:45 +0300

    MAC-62 Production mode implementation (update)

--- Model/Config.php
+++ Model/Config.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 namespace Aheadworks\MobileAppConnector\Model;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Store\Model\ScopeInterface;
 
 /**
  * Class Config of system settings
  */
 class Config
 {
-    public const XML_PATH_PRODUCTION_MODE_ENABLED = 'aw_mac/aw_mac_setting/production_mode_enabled';
+    public const XML_PATH_PRODUCTION_MODE_ENABLED = 'aw_mac/aw_mac_setting/is_production_mode_enabled';
 
     /**
      * @var ScopeConfigInterface
@@ -31,15 +30,13 @@ class Config
     /**
      * Is Production module enabled
      *
-     * @param int|null $storeId
      * @return bool
      */
-    public function isProductionModeEnabled($storeId = null): bool
+    public function isProductionModeEnabled(): bool
     {
         return $this->scopeConfig->isSetFlag(
             self::XML_PATH_PRODUCTION_MODE_ENABLED,
-            ScopeInterface::SCOPE_STORE,
-            $storeId
+            ScopeConfigInterface::SCOPE_TYPE_DEFAULT
         );
     }
 }
