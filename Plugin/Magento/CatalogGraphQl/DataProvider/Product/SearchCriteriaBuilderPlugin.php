339c439d3608f24d521ca21992f6639afd96f8fb
Yuri Silchenkov <silchenkov@aheadworks.com>
2022-09-08 14:57:49 +0300

    MAC-71 Add visibility filter to QraphQL queries

---
+++ Plugin/Magento/CatalogGraphQl/DataProvider/Product/SearchCriteriaBuilderPlugin.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace Aheadworks\MobileAppConnector\Plugin\Magento\CatalogGraphQl\DataProvider\Product;
+
+use Magento\CatalogGraphQl\DataProvider\Product\SearchCriteriaBuilder as Subject;
+use Magento\Framework\Api\Search\SearchCriteriaInterface;
+use Aheadworks\MobileAppConnector\Model\Service\SearchCriteriaEditor;
+
+/**
+ * Class SearchCriteriaBuilderPlugin for apply custom filter
+ */
+class SearchCriteriaBuilderPlugin
+{
+    public const VISIBILITY_FIELD_FILTER = 'visibility';
+
+    /**
+     * @var SearchCriteriaEditor
+     */
+    private $searchCriteriaEditor;
+
+    /**
+     * SearchCriteriaBuilderPlugin constructor.
+     *
+     * @param SearchCriteriaEditor $searchCriteriaEditor
+     */
+    public function __construct(SearchCriteriaEditor $searchCriteriaEditor)
+    {
+        $this->searchCriteriaEditor = $searchCriteriaEditor;
+    }
+
+    /**
+     * Build search criteria according custom filter
+     *
+     * @param Subject $subject
+     * @param callable $proceed
+     * @param array $args
+     * @param bool $includeAggregation
+     * @return SearchCriteriaInterface
+     */
+    public function aroundBuild(
+        Subject $subject,
+        callable $proceed,
+        array $args,
+        bool $includeAggregation
+    ): SearchCriteriaInterface {
+        $isVisibilityFilterIncluded = isset($args['filter'][self::VISIBILITY_FIELD_FILTER]);
+
+        if ($isVisibilityFilterIncluded) {
+            $conditionFilter = array_key_first($args['filter'][self::VISIBILITY_FIELD_FILTER]);
+            $valueFilter = $args['filter'][self::VISIBILITY_FIELD_FILTER][$conditionFilter];
+        }
+
+        /** @var SearchCriteriaInterface $searchCriteria */
+        $searchCriteria = $proceed($args, $includeAggregation);
+
+        if ($isVisibilityFilterIncluded) {
+            $this->searchCriteriaEditor->clearSimilarFilters(
+                $searchCriteria,
+                self::VISIBILITY_FIELD_FILTER,
+                $conditionFilter,
+                $valueFilter
+            );
+        }
+
+        return $searchCriteria;
+    }
+}
